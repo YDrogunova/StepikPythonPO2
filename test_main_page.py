93ba106440df5a02cbf39f8f8b897e2f1d3249f1
YDrogunova <juli11dr@gmail.com>
2020-04-22 14:39:30 +0300

    add login page cheks

--- test_main_page.py
+++ test_main_page.py
@@ -7,7 +7,7 @@ from pages.main_page import MainPage
     #page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
 
 def test_guest_should_see_login_link(browser):
-        link = "http://selenium1py.pythonanywhere.com/"
+        link = "http://selenium1py.pythonanywhere.com/ru/"
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
